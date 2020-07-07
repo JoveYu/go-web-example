c9ae55f30959d5e961b10e5f417cf5ec3f75296c
Jove Yu <yushijun110@126.com>
2020-07-07 09:42:11 +0800

    update validator

--- common/util/validator.go
+++ common/util/validator.go
@@ -1,6 +1,9 @@
 package util
 
 import (
+	"errors"
+	"strings"
+
 	"github.com/go-playground/validator/v10"
 
 	"github.com/gin-gonic/gin/binding"
@@ -14,7 +17,7 @@ func init() {
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
 		en := en.New()
 		zh := zh.New()
-		uni := ut.New(zh, en)
+		uni := ut.New(en, zh)
 		trans, _ = uni.GetTranslator("zh")
 		_ = zhtranslations.RegisterDefaultTranslations(v, trans)
 	}
@@ -23,13 +26,17 @@ func init() {
 var trans ut.Translator
 
 func WrapValidationError(err error) string {
-	result := err.Error()
-	if err, ok := err.(validator.ValidationErrors); ok {
-		errs := err.Translate(trans)
-		result = ""
+	var result string
+	verr := validator.ValidationErrors{}
+	if errors.As(err, &verr) {
+		errs := verr.Translate(trans)
+		strs := []string{}
 		for _, v := range errs {
-			result += v + ","
+			strs = append(strs, v)
 		}
+		result = strings.Join(strs, ",")
+	} else {
+		result = err.Error()
 	}
 	return result
 }
